9856fe7b0e3ffefb78ec94ac04be251bfcf07ed8
Dolzy <dolzy@LAPTOP-SOLC3NM9.localdomain>
2022-07-23 06:00:20 +0100

    modified code

---
+++ 0x05-processes_and_signals/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * main - Creates five zombie processes.
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char count = 0;
+    printf("%c", count);
+    printf("%d", count);
+    return 0;
+}
