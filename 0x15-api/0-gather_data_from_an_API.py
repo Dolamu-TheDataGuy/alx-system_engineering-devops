67d3c1c8d47672b3f6d553135118fe7f28afb169
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2022-12-10 06:03:53 +0100

    added file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,34 @@
 #!/usr/bin/python3
-"""Returns a TO-DO list information for a given employee ID."""
+"""
+gathers information about from api
+about an employee by ID and returns
+"""
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+def get_info():
+    """the method to get info"""
+    url = 'https://jsonplaceholder.typicode.com/'
+    name_url = '{}users/{}'.format(url, sys.argv[1])
+    name = requests.get(name_url).json().get('name')
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tasks = tasks.json()
+    complete = 0
+    titles = []
+    total = 0
+    for task in tasks:
+        if task['userId'] == int(sys.argv[1]):
+            if task['completed'] is True:
+                complete += 1
+                titles.append(task['title'])
+            total += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, complete, total))
+    for title in titles:
+        print('\t ', end="")
+        print(title)
+
+
+if __name__ == "__main__":
+    get_info()
